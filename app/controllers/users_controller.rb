1296440f0f06dcfed748b308e6b60db0f5504a70
chinaclp <1048900587@qq.com>
2014-11-05 18:18:39 +0800

    index分页

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+ #   @users = User.all
+    @users = User.order('id desc').page(params[:page]).per(3)
   end
 
   def new
